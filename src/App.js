14a62aa480e7c524854be588e82a8d52f72ecff4
Jonathan Caburnay <jmcaburnay1@up.edu.ph>
2021-03-12 23:33:27 +0800

    Exercise 2.6: The Phonebook Step1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const Persons = ({ persons }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ul>
+      {persons.map(person => <li key={person.name}>{person.name}</li>)}
+    </ul>
+  )
+} 
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const submit = event => {
+    event.preventDefault();
+    const nameObject = {
+      name: newName,
+    }
+    setPersons(persons.concat(nameObject));
+    setNewName('');
+  }
+
+  const handleNameChange = event => {
+    setNewName(event.target.value);
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={submit}>
+        <div>
+          name: <input type="text" value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <Persons persons={persons} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
