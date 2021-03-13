09c92147a25be29c437d118f0531028d381b2c20
Jonathan Caburnay <jmcaburnay1@up.edu.ph>
2021-03-13 21:25:08 +0800

    Exercise 2.12*: Data for countries, step1

--- src/App.js
+++ src/App.js
@@ -1,119 +1,63 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const Filter = ({ searchName, onSearch }) => {
-  return (
-    <div>
-      <span>
-        search contact:{" "}
-        <input type="text" value={searchName} onChange={onSearch} />
-      </span>
-    </div>
-  );
-};
-
-const PersonForm = ({
-  submit,
-  newName,
-  handleNameChange,
-  newNumber,
-  handleNumberChange,
-}) => {
-  return (
-    <form onSubmit={submit}>
-      <div>
-        name:{" "}
-        <input
-          type="text"
-          value={newName}
-          onChange={handleNameChange}
-          required
-        />
-      </div>
-      <div>
-        number:{" "}
-        <input
-          type="text"
-          value={newNumber}
-          onChange={handleNumberChange}
-          required
-        />
-      </div>
-      <div>
-        <button type="submit">add</button>
-      </div>
-    </form>
-  );
-};
-
-const Persons = ({ persons }) => {
+const Country = ({ countries }) => {
+  if(countries.length > 10) {
+    return (
+      <p>Too many matches, specify another filter</p>
+    )
+  } else if(countries.length === 1) {
+    return (
+      <>
+        {countries.map(country => {
+          return (
+            <React.Fragment key={country.name}>
+              <h2>{country.name}</h2>
+              <p>capital: {country.capital}</p>
+              <p>population: {country.population}</p>
+              <h4>languages: </h4>
+              <ul>
+                {country.languages.map(language => <li key={language.name}>{language.name}</li>)}
+              </ul>
+              <img src={country.flag} alt={`${country.name} flag`}/>
+            </React.Fragment>
+          )
+        })}
+      </>
+    )
+  }
   return (
     <ul>
-      {persons.map((person) => (
-        <li key={person.name}>
-          {person.name} {person.number}
-        </li>
-      ))}
+      {countries.map(country => <li key={country.name}>{country.name}</li>)}
     </ul>
-  );
-};
+  )
+}
 
 const App = () => {
-  const [persons, setPersons] = useState([]);
-  const [newName, setNewName] = useState("");
-  const [newNumber, setNewNumber] = useState("");
-  const [searchName, setSearchName] = useState("");
+  const [countries, setCountries] = useState([]);
+  const [searchCountry, setSearchCountry] = useState("");
 
   useEffect(() => {
-    axios.get("http://localhost:3001/persons").then((response) => {
-      setPersons(response.data);
-    });
-  }, []);
-
-  const submit = (event) => {
-    event.preventDefault();
-    if (persons.map((person) => person.name).includes(newName)) {
-      alert(`${newName} is already in the phonebook`);
+    if(searchCountry === "") {
+      setCountries([]);
     } else {
-      const nameObject = {
-        name: newName,
-        number: newNumber,
-      };
-      setPersons(persons.concat(nameObject));
-      setNewName("");
-      setNewNumber("");
+      axios.get("https://restcountries.eu/rest/v2/all").then((response) => {
+        setCountries(response.data);
+      });
     }
-  };
-
-  const handleNameChange = (event) => {
-    setNewName(event.target.value);
-  };
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value);
-  };
+  }, [searchCountry]);
 
   const onSearch = (event) => {
-    setSearchName(event.target.value);
+    setSearchCountry(event.target.value);
   };
 
-  const searchedNames = persons.filter((person) =>
-    person.name.toLowerCase().includes(searchName.toLowerCase())
+  const searchedCountries = countries.filter((country) =>
+    country.name.toLowerCase().includes(searchCountry.toLowerCase())
   );
-
   return (
     <div>
-      <h2>Phonebook</h2>
-      <Filter searchName={searchName} onSearch={onSearch} />
-      <h3>Add a new contact</h3>
-      <PersonForm
-        submit={submit}
-        newName={newName}
-        handleNameChange={handleNameChange}
-        newNumber={newNumber}
-        handleNumberChange={handleNumberChange}
-      />
-      <h3>Numbers</h3>
-      <Persons persons={searchedNames} />
+      <p>find countries: <input type="text" value={searchCountry} onChange={onSearch}/></p>
+      <Country countries={searchedCountries} />
     </div>
   );
 };
