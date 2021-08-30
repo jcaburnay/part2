3e83b4e2669e517d0c7ffc026dabf23099a287d4
Jonathan Caburnay <jmcaburnay1@up.edu.ph>
2021-08-30 17:53:10 +0800

    Exercise 2.16: Phonebook step8

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import personService from "./services/persons";
 
 const Filter = ({ searchName, onSearch }) => {
   return (
@@ -65,9 +65,11 @@ const App = () => {
   const [searchName, setSearchName] = useState("");
 
   useEffect(() => {
-    axios.get("http://localhost:3001/persons").then((response) => {
-      setPersons(response.data);
-    });
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
+      });
   }, []);
 
   const submit = (event) => {
@@ -79,9 +81,10 @@ const App = () => {
         name: newName,
         number: newNumber,
       };
-      axios.post("http://localhost:3001/persons", personObject)
-        .then(response => {
-          setPersons(persons.concat(response.data));
+      personService
+        .add(personObject)
+        .then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson));
           setNewName("");
           setNewNumber("");
         })
